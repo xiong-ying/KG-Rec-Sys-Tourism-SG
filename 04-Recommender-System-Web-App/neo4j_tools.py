e89d3d23c59e4b7487ceaed50192fc133471c160
xiong ying <98197209+xiong-ying@users.noreply.github.com>
2024-05-10 17:57:02 +0800

    Update neo4j_tools.py

--- 04-Recommender-System-Web-App/neo4j_tools.py
+++ 04-Recommender-System-Web-App/neo4j_tools.py
@@ -7,10 +7,18 @@ import configparser
 
 # FUNCTION: get neo4j connection credential from config file or default value
 def get_credential():
+
     # Using an ini file for credentials, otherwise providing defaults
     HOST = 'neo4j://localhost'
     DATABASE = 'neo4j'
     PASSWORD = 'password'
+    
+    '''
+    # enter sandbox credentials manually, to be removed later.
+    HOST = 'bolt://3.86.237.237:7687'
+    DATABASE = 'neo4j'
+    PASSWORD = 'terminators-coal-basins'
+    '''
 
     NEO4J_CONF_FILE = 'neo4j.ini'
     '''
